8541a585cb69c0d7431a351dde24027664d6410f
Verson-tech <77786047+Verson-tech@users.noreply.github.com>
2021-06-08 14:41:24 +0300

    Update manipulation.js

--- manipulation.js
+++ manipulation.js
@@ -1,45 +1,57 @@
 const busStops = [
-    [-71.093729, 42.359244],
-    [-71.094915, 42.360175],
-    [-71.0958, 42.360698],
-    [-71.099558, 42.362953],
-    [-71.103476, 42.365248],
-    [-71.106067, 42.366806],
-    [-71.108717, 42.368355],
-    [-71.110799, 42.369192],
-    [-71.113095, 42.370218],
-    [-71.115476, 42.372085],
-    [-71.117585, 42.373016],
-    [-71.118625, 42.374863],
-  ];
-  
+  [-71.093729, 42.359244],
+  [-71.094915, 42.360175],
+  [-71.0958, 42.360698],
+  [-71.099558, 42.362953],
+  [-71.103476, 42.365248],
+  [-71.106067, 42.366806],
+  [-71.108717, 42.368355],
+  [-71.110799, 42.369192],
+  [-71.113095, 42.370218],
+  [-71.115476, 42.372085],
+  [-71.117585, 42.373016],
+  [-71.118625, 42.374863],
+];
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoidmVyc29uLXRlY2giLCJhIjoiY2tvaWMzYmdxMWU3OTMxdHJodzF2dHp4ciJ9.JO8-pT2PnSAc0Xf99JejGw';
-  
 
-  let map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-71.104081, 42.365554],
-    zoom: 14,
-  });
-  
-  
-  let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
+mapboxgl.accessToken = 'pk.eyJ1IjoidmVyc29uLXRlY2giLCJhIjoiY2tvaWJ5Z3RuMGpjbTMyczFib2F6eDg3MiJ9.0P__eXNxNw5FaClyThj8-A';
 
-  let counter = 0;
-  function move() {
-   
-      setTimeout(() => {
-      if (counter >= busStops.length) return;
-      marker.setLngLat(busStops[counter]);
-      counter++;
-      move();
-    }, 1000);
-  }
-  
 
-  if (typeof module !== 'undefined') {
-    module.exports = { move };
-  }
-  
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-71.104081, 42.365554],
+  zoom: 14,
+});
+
+
+let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
+
+async function getBusLocation(){
+  const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+  const response = await fetch(url);
+  const json = await response.json();
+
+  let data = json.data;
+  let bus = data[0].attributes;
+  let longitude = bus.longitude;
+  let latitude = bus.latitude;
+
+  return [longitude, latitude];
+}
+
+let counter = 0;
+async function move() {
+
+  let location = await getBusLocation();
+
+  marker.setLngLat(location);
+ 
+  setTimeout(move, 15000);
+}
+if (typeof module !== 'undefined') {
+  module.exports = { move };
+}
+
+
+
